17b26144d03fc30a75db813811a21575ab93f3f4
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-27 17:59:13 -0700

    Setup initial CreationZone structure

--- src/CreationZone/CreationZone.js
+++ src/CreationZone/CreationZone.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react'
 import './CreationZone.scss'
+import { NavLink } from 'react-router-dom';
 
 class CreationZone extends Component {
   constructor() {
     super();
     this.state = {
-
+        name: '',
+        memberOne: '',
+        memberTwo: '',
+        memberThree: '',
     }
   }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
     return (
-      <section>
-        <h1>Creation Zone</h1>
-      </section>
+      <>
+        <section className='creation-zone'>
+          <h1 className='creation-title'>Creation Zone</h1>
+          <label className='team-name-label'>Team Name:</label>
+          <input 
+            name='name'
+            value={this.state.name}
+            className='team-name-input'
+            placeholder='Team Name'
+            onChange={(e) => this.handleChange(e)}
+          />
+          <div className='member-inputs'>
+            <label className='member-label'>Team Member 1:
+            <input 
+              name='memberOne'
+              value={this.state.memberOne}
+              className='member-input'
+              placeholder='Team Member 1'
+              onChange={(e) => this.handleChange(e)}
+            />
+            </label>
+            <label className='member-label'>Team Member 2:
+            <input 
+              name='memberTwo'
+              value={this.state.memberTwo}
+              className='member-input'
+              placeholder='Team Member 2'
+              onChange={(e) => this.handleChange(e)}
+            />
+            </label>
+            <label className='member-label'>Team Member 3:
+            <input 
+              name='memberThree'
+              value={this.state.memberThree}
+              className='member-input'
+              placeholder='Team Member 3'
+              onChange={(e) => this.handleChange(e)}
+            />
+            </label>
+          </div>
+          <NavLink className='build-btn' type='button' to='/team'>Build</NavLink>
+        </section>
+      </>
     )
   }
 }
+
 
 export default CreationZone;
