b0c966ffc0b998cd99bc49d1a88e9b581a1078c6
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 14:37:00 -0700

    Add deleteFavorite action to FavoriteCard

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteFavorite } from '../Actions'
 
-export const FavoriteCard = ({team}) => {
-
+export const FavoriteCard = ({team, deleteFavorite}) => {
   return (
     <div>
       <h2>{team.name}</h2>
       <img className='member-image' src={team.memberOne.image.url} alt='member one'/>
       <img className='member-image' src={team.memberTwo.image.url} alt='member two'/>
       <img className='member-image' src={team.memberThree.image.url} alt='member three'/>
-      <button>Delete</button>
+      <button onClick={() => deleteFavorite(team.id)}>Delete</button>
     </div>
   )
 }
 
-export default FavoriteCard
+export const mapDispatchToProps = dispatch => ({
+  deleteFavorite: id => dispatch(deleteFavorite(id)),
+})
+
+export default connect(null, mapDispatchToProps)(FavoriteCard)
