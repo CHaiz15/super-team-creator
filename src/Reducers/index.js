abd669919c5f765e6deadf5f08cd3cde388e4369
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 14:05:04 -0700

    Add favReducer to rootReducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { teamReducer } from './teamReducer'
+import { favReducer } from './favReducer'
 
 const rootReducer = combineReducers({
   team: teamReducer,
+  favorites: favReducer,
 });
 
 export default rootReducer;
