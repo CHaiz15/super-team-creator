c68a03372f9695f9455bb489f15d3929851ed241
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 15:55:03 -0700

    Add Link to navigate to FavTeamDisplay

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { deleteFavorite } from '../Actions'
+import './FavoriteCard.scss'
+import FavTeamDisplay from '../FavTeamDisplay/FavTeamDisplay'
 
 export const FavoriteCard = ({team, deleteFavorite}) => {
+
   return (
-    <div>
-      <h2>{team.name}</h2>
-      <img className='member-image' src={team.memberOne.image.url} alt='member one'/>
-      <img className='member-image' src={team.memberTwo.image.url} alt='member two'/>
-      <img className='member-image' src={team.memberThree.image.url} alt='member three'/>
+    <>
+      <Link to={{pathname:'/favTeam', state: {team}}}> 
+        <div className='fav-card'>
+          <h2>{team.name}</h2>
+          <img className='member-image' src={team.memberOne.image.url} alt='member one'/>
+          <img className='member-image' src={team.memberTwo.image.url} alt='member two'/>
+          <img className='member-image' src={team.memberThree.image.url} alt='member three'/>
+        </div>
+      </Link>
       <button onClick={() => deleteFavorite(team.id)}>Delete</button>
-    </div>
+    </>
   )
 }
 
