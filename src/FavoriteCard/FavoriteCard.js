982846f28fd4e090ef2c31a5784e772d1aa51919
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 14:03:30 -0700

    Add initial FavoriteCard structure

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export const FavoriteCard = () => {
+export const FavoriteCard = ({team}) => {
+console.log(team.name);
+
   return (
     <div>
-      <h2>Team Name:</h2>
+      <h2>{team.name}</h2>
+      <img className='member-image' src={team.memberOne.image.url} alt='member one'/>
+      <img className='member-image' src={team.memberTwo.image.url} alt='member two'/>
+      <img className='member-image' src={team.memberThree.image.url} alt='member three'/>
+      <button>Delete</button>
     </div>
   )
 }
