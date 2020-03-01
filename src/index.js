f2b43b2ee01d7e299a0da61e9c8373791499d69c
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 16:09:42 -0700

    Clean index.js for formatting inconsistencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App/App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
-import rootReducer from './Reducers/index';
+import rootReducer from './Reducers/index'
 
 const store = createStore(rootReducer, composeWithDevTools())
 
@@ -17,8 +17,8 @@ const router = (
       <App />
     </BrowserRouter>
   </Provider>
-);
+)
 
-ReactDOM.render(router, document.getElementById('root'));
+ReactDOM.render(router, document.getElementById('root'))
 
-serviceWorker.unregister();
+serviceWorker.unregister()
