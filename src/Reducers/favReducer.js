0249e3af1963e4d426089db87746759069b2a512
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 14:37:40 -0700

    Add DELETE_FAVORITE case to favReducer

--- src/Reducers/favReducer.js
+++ src/Reducers/favReducer.js
@@ -2,7 +2,10 @@ export const favReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_FAVORITE':
       return [...state, action.team]
+    case 'DELETE_FAVORITE':
+      let favorites = state.filter(favTeam => favTeam.id !== action.id)
+      return favorites
     default:
-      return state;
+      return state
   }
 }
