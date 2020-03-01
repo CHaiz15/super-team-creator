5e410a72d2692967a3990e28093430ada7356794
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 12:33:47 -0700

    Add Favorites inital structure

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Favorites.scss'
+import FavoriteCard from '../FavoriteCard/FavoriteCard'
+
+export const Favorites = () => {
+  // const favoriteCards = favorites.map(favoriteTeam => <FavoriteCard />)
+
+  return (
+    <section>
+      <h1 className='creation-title'>Favorites</h1>
+      {/* {favoriteCards} */}
+    </section>
+  )
+}
+
+export default Favorites
+
