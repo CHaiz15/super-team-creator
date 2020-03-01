1bf4557de2076601182235a6fef6675b7c77eb58
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 14:04:04 -0700

    Add logic to display all favorited teams

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import './Favorites.scss'
 import FavoriteCard from '../FavoriteCard/FavoriteCard'
+import { connect } from 'react-redux'
 
-export const Favorites = () => {
-  // const favoriteCards = favorites.map(favoriteTeam => <FavoriteCard />)
-
+export const Favorites = ({favorites}) => {
+  const favoriteCards = favorites.map(favoriteTeam => <FavoriteCard team={favoriteTeam} />)
   return (
     <section>
       <h1 className='creation-title'>Favorites</h1>
-      {/* {favoriteCards} */}
+      {favoriteCards}
     </section>
   )
 }
 
-export default Favorites
+export const mapStateToProps = state => ({
+  favorites: state.favorites,
+})
+
+export const mapDispatchToProps = dispatch => ({
+  // addFavorite: team => dispatch(addFavorite(team)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
 
