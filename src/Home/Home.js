523c5988404936881e72d27d16a3211bb188f29e
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-01 21:59:04 -0700

    Remove favorites nav button from Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,9 +6,8 @@ export const Home = () => {
   return (        
     <main className="Home">
       <div>
-        <h1 className='main-title'>Super Team Creator</h1>
-        <NavLink type='button' to='/favorites' className='fav-btn'>Favorites</NavLink>
       </div>
+        <h1 className='main-title'>Super Team Creator</h1>
       <div className='instruction-box'>
         <h3 className='instructions-title'>Instructions</h3>
         <p className='instructions'>Have you ever wanted to create your own super team of heros and villians? Well now you can with the Super Team Creator! Just type in 3 of your favorite heros or villians. Give your team a heroic name and checkout what their stats are! Try all types of combinations and save your favorites!</p>
