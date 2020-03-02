0fe3dced5483b53a4da2171b87c0c50d31a949dd
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 10:04:27 -0700

    Add TeamDisplay mapDispatchToProps test

--- src/TeamDisplay/TeamDisplay.test.js
+++ src/TeamDisplay/TeamDisplay.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { TeamDisplay } from './TeamDisplay.js';
+import { TeamDisplay } from './TeamDisplay.js'
 import { shallow } from 'enzyme'
+import { mapStateToProps, mapDispatchToProps } from './TeamDisplay'
+import { addFavorite } from '../actions'
 
 describe('TeamDisplay', () => {
   let wrapper;
@@ -50,5 +52,57 @@ describe('TeamDisplay', () => {
   })  
   it('should be an instance of the TeamDisplay component', () => {
       expect(wrapper).toMatchSnapshot();
-  });
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with a addFavorite action when Add to Favorites button is clicked', () => {
+      const mockTest = {
+        name:'Best Team Ever',
+        memberOne: {
+          name:'Hulk', 
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberTwo: {
+          name:'Thanos',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberThree: {
+          name:'Batman',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        id: 123,
+      }
+      const mockDispatch = jest.fn();
+      const actionToDispatch = addFavorite(mockTest);
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.addFavorite(mockTest);
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  })
 })
