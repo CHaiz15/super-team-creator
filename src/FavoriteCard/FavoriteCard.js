15053415ed704d65d952a291ec7010426a26e89e
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 16:40:12 -0700

    Updated FavoriteCard layout

--- src/FavoriteCard/FavoriteCard.js
+++ src/FavoriteCard/FavoriteCard.js
@@ -10,13 +10,13 @@ export const FavoriteCard = ({team, deleteFavorite}) => {
   return (
     <>
       <div className='fav-card'>
-        <h2>{team.name}</h2>
         <Link to={{pathname:'/favTeam', state: {team}}}> 
+        <h2 className='fav-team-title'>{team.name}</h2>
         <img className='card-member-image' src={team.memberOne.image.url} alt='member one'/>
         <img className='card-member-image' src={team.memberTwo.image.url} alt='member two'/>
         <img className='card-member-image' src={team.memberThree.image.url} alt='member three'/>
         </Link>
-        <button onClick={() => deleteFavorite(team.id)}>Delete</button>
+        <button className='delete-btn' onClick={() => deleteFavorite(team.id)}>Delete</button>
       </div>
     </>
   )
