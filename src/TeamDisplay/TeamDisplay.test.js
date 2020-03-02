f58788fdcba1f5190e2b501484e6d93d6e1e94fc
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 10:13:38 -0700

    Add TeamDisplay mapStateToProps test

--- src/TeamDisplay/TeamDisplay.test.js
+++ src/TeamDisplay/TeamDisplay.test.js
@@ -105,4 +105,171 @@ describe('TeamDisplay', () => {
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     })
   })
+
+  describe('mapStateToProps', () => {
+    it('should return an object with an array of movies', () => {
+      const mockState = {
+        team: {
+          name:'Best Team Ever',
+          memberOne: {
+            name:'Hulk', 
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          memberTwo: {
+            name:'Thanos',
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          memberThree: {
+            name:'Batman',
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          id: 123,
+        },
+        name: 'Best Team Ever',
+        memberOne: {
+          name:'Hulk', 
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberTwo: {
+          name:'Thanos',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberThree: {
+          name:'Batman',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+      }
+      
+      const expected = {
+        team: {
+          name:'Best Team Ever',
+          memberOne: {
+            name:'Hulk', 
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          memberTwo: {
+            name:'Thanos',
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          memberThree: {
+            name:'Batman',
+            powerstats: {
+              Intelligence: 81,
+              strength: 40,
+              Speed: 29,
+              Durability: 55,
+              Power: 63,
+              Combat: 90,
+            },
+            image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+          },
+          id: 123,
+        },
+        name: 'Best Team Ever',
+        memberOne: {
+          name:'Hulk', 
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberTwo: {
+          name:'Thanos',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+        memberThree: {
+          name:'Batman',
+          powerstats: {
+            Intelligence: 81,
+            strength: 40,
+            Speed: 29,
+            Durability: 55,
+            Power: 63,
+            Combat: 90,
+          },
+          image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+        },
+      }
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected)
+    })
+  })
 })
