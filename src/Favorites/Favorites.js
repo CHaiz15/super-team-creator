c1451f09302a17ed0f9127d2ca05f476babe324f
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 10:03:57 -0700

    Add conditional logic to alert user if 0 favorites

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -4,10 +4,12 @@ import FavoriteCard from '../FavoriteCard/FavoriteCard'
 import { connect } from 'react-redux'
 
 export const Favorites = ({favorites}) => {
+
   const favoriteCards = favorites.map(favoriteTeam => <FavoriteCard key={favoriteTeam.id} team={favoriteTeam} />)
   return (
     <section className='fav-cards-container'>
       <h1 className='fav-title'>Favorites</h1>
+      <h1 className={favorites.length === 0 ? 'no-favorites':'hidden'}>No favorites... Make some!</h1>
       {favoriteCards}
     </section>
   )
