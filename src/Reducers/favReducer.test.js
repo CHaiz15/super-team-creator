2758a5cea1f4eef20c7d346506d52f3b523caacb
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 00:31:00 -0700

    Add correct state test if type is ADD_FAVORITE

--- src/Reducers/favReducer.test.js
+++ src/Reducers/favReducer.test.js
@@ -6,3 +6,97 @@ it('should return the initial state if there is no state provided', () => {
   expect(result).toEqual(expected)
 })
 
+it('should return the correct state if the action type is ADD_FAVORITE', () => {
+  const team = { 
+    name:'Best Team Ever',
+    memberOne: {
+      name:'Hulk', 
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberTwo: {
+      name:'Thanos',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberThree: {
+      name:'Batman',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    id: 123,
+  }
+
+  const mockState = [];
+
+  const mockAction = {
+    type: 'ADD_FAVORITE',
+    team,
+  }
+
+  const result = favReducer(mockState, mockAction)
+
+  const expected = [{
+    name:'Best Team Ever',
+    memberOne: {
+      name:'Hulk', 
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberTwo: {
+      name:'Thanos',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberThree: {
+      name:'Batman',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    id: 123,
+  }]
+
+  expect(result).toEqual(expected)
+})
