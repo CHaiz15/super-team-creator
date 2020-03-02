fe37bd63572138d5c52b098176cde6a949e8a036
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-02 00:38:13 -0700

    Add correct state test if type is DELETE_FAVORITE

--- src/Reducers/favReducer.test.js
+++ src/Reducers/favReducer.test.js
@@ -100,3 +100,60 @@ it('should return the correct state if the action type is ADD_FAVORITE', () => {
 
   expect(result).toEqual(expected)
 })
+
+it('should return the correct state if the action type is DELETE_FAVORITE', () => {
+  const favorites = {
+    name:'Best Team Ever',
+    memberOne: {
+      name:'Hulk', 
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberTwo: {
+      name:'Thanos',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    memberThree: {
+      name:'Batman',
+      powerstats: {
+        Intelligence: 81,
+        strength: 40,
+        Speed: 29,
+        Durability: 55,
+        Power: 63,
+        Combat: 90,
+      },
+      image: {url: "https://www.superherodb.com/pictures2/portraits/10/100/83.jpg"},
+    },
+    id: 123
+  }
+
+  const mockAction = {
+    type: 'DELETE_FAVORITE',
+    favorites,
+  }
+
+  const mockState = []
+
+  const result = favReducer(mockState, mockAction)
+
+  const expected = []
+
+  expect(result).toEqual(expected)
+})
+
